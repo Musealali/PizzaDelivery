121047f952234ab3e09cd29667df663981fd315f
thejokerd3 <muslimalali@hotmail.com>
2023-06-01 14:20:58 +0200

    adding custom pizza implementation

--- PizzaDeliveryRazorPages/PizzaDeliveryRazorPages/Pages/Forms/CustomPizza.cshtml.cs
+++ PizzaDeliveryRazorPages/PizzaDeliveryRazorPages/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaDeliveryRazorPages.Models;
 
 namespace PizzaDeliveryRazorPages.Pages.Forms;
 
 public class CustomPizza : PageModel
 {
+    [BindProperty]
+    public PizzasModel Pizza { get; set; }
     public void OnGet()
     {
         
